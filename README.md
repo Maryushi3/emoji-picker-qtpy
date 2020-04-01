3faa24e0e7bc801b9db0e0c2c7afc1547f1a1058
Mariusz Kozłowski <maryushi3@gmail.com>
2020-04-01 15:53:39 +0200

    Added main script and updated the readme

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # emoji-picker-qtpy
-PyQt5-based emoji picker
+PyQt5-🅱️ased emoji picker
+
+## Why? 🤔
+
+Because KDE Plasma's _Emoji Picker_ (`plasma.emojier`) neither goes to search textbox automatically nor allows user to navigate using arrow keys 🤦
+
+## What it does? (features) 🤸
+
+- alows to search emoji by name
+- copies selected emoji to system clipboard
+- immediately pastes selected emoji, assuming the picker was summoned with a text box focused
+- closes on focus lost, `Esc` key pressed or emoji selected
+- displayed emoji count is limited by how many of them fit inside the widget
+
+For now it's possible to select emoji only using mouse pointer. Keyboard support coming soon!
+
+## Dependencies 🧰
+
+This project depends largery on [emoji_data_python](https://github.com/alexmick/emoji-data-python). This will likely get replaced by [emoji.json](https://github.com/github/gemoji/blob/master/db/emoji.json) since it contains more emoji and has more predictable names.
+
+To install (almost) all dependencies run
+```
+pip install pyqt5 emoji_data_python Xlib pyautogui
+```
+
+Last dependency is the [Tkinter](https://docs.python.org/2/library/tkinter.html). 
+
+Arch Linux users can install Tkinter by installing `tk` package from `extra` repository. While debian-based distro users should install `python3-tk` package.
+
+## Serving suggestion 🥘
+
+Create an executable shell script in an accessible location with following contents:
+```
+#!/bin/bash
+python /(full path to directory containing the file)/emoji-picker-qt.py
+```
+and bind it to a keyboard shortcut (eg. using _Custom Shortcuts_ window in KDE Plasma).
+
+## Planned features (in no particular order)
+
+- arrow navigation (main priority)
+- fuzzy search
+- settings  (or just `.config` support)
+- slack-style search (eg. searching for `:joy:` would give 😂)
+- category tabs
---
+++ emoji-picker-qt.py
@@ -0,0 +1,166 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import emoji_data_python as edp
+from Xlib import display
+import pyautogui
+import time
+from PyQt5 import QtTest
+
+# globals
+emojiGridLayout = None
+mainWindow = None
+emojiGridColumnCount = 7
+emojiGridRowCount = 4
+emojiFontSize = 20
+willExitOnItsOwn = False
+
+font = QFont()
+font.setPointSize(emojiFontSize)
+
+# quits without a lag
+def quitNicely():
+    mainWindow.hide()
+    quit()
+
+
+# gets mouse position from Xlib
+def mousePosition():
+    pointerData = display.Display().screen().root.query_pointer()._data
+    return pointerData["root_x"], pointerData["root_y"]
+
+# copies and pastes selected emoji
+def emojiClicked(char):
+    global willExitOnItsOwn
+    willExitOnItsOwn = True
+    mainWindow.hide()
+    QApplication.clipboard().setText(char)
+    pyautogui.hotkey("ctrl","v")
+    QtTest.QTest.qWait(250)
+    quit()
+
+# searches for emoji, and fills grid with labels
+def execute_search(text):
+
+    if not text or text.isspace():
+        return
+
+    global emojiGridLayout
+    for i in reversed(range(emojiGridLayout.count())): 
+        emojiGridLayout.itemAt(i).widget().setParent(None)
+
+    rowIdx = 0
+    colIdx = 0
+
+    for emoji in edp.find_by_name(text):
+        if rowIdx>emojiGridRowCount-1:
+            break;
+
+        label = QClickableLabel(emoji.char)
+        label.clicked.connect(emojiClicked)
+        label.setFont(font)
+        label.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
+        emojiGridLayout.addWidget(label,rowIdx,colIdx)
+        emojiGridLayout.setAlignment(label,Qt.AlignTop)
+        if colIdx < emojiGridColumnCount-1:
+            colIdx+=1
+        else:
+            colIdx=0
+            rowIdx+=1
+
+def on_key(key):
+    # test for a specific key
+    if key == Qt.Key_Escape:
+        quitNicely()
+
+# main app window class with inits
+class EmojiPickerWindow(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        # focus handling
+        self.installEventFilter(self)
+
+        self.title = 'Emoji picker ＼(^o^)／'
+        self.width = 274
+        self.height = 250
+
+        # start with text box centered at mouse pointer position
+        self.left, self.top = mousePosition()  
+        self.left -= self.width//2
+        self.top += (24-self.height)
+
+        self.initUI()
+        
+    def initUI(self):
+        # topmost window layout 
+        layout = QVBoxLayout() 
+
+        # scroll area setup shenanigans
+        scrollArea = QScrollArea() 
+        gridWidget = QWidget()
+        global emojiGridLayout
+        emojiGridLayout = QGridLayout(gridWidget)
+        emojiGridLayout.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+        gridWidget.setStyleSheet("QLabel:hover{background-color:palette(highlight);}")
+        scrollArea.setWidget(gridWidget)
+        scrollArea.setWidgetResizable(True)
+        layout.addWidget(scrollArea)
+
+        # fill with a placeholder for now (smiling or smile)
+        execute_search('smil')
+
+        # bottom text entry
+        lineEdit = QLineEdit() 
+        lineEdit.textChanged.connect(execute_search)
+        layout.addWidget(lineEdit)
+
+        # align it to the bottom, so that it won't stay centered vertically
+        layout.setAlignment(lineEdit, Qt.AlignBottom)
+
+
+        self.setLayout(layout)
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setFixedSize(self.width, self.height)
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
+
+        # needed for filling the grid out from the outside
+        global mainWindow
+        mainWindow = self
+
+        # esc handling
+        self.keyPressed.connect(on_key)
+
+        self.show()
+        lineEdit.setFocus()
+
+    # esc handling
+    keyPressed = pyqtSignal(int)
+    def keyPressEvent(self, event):
+        super(EmojiPickerWindow, self).keyPressEvent(event)
+        self.keyPressed.emit(event.key())
+
+    # focus handling
+    global willExitOnItsOwn
+    def eventFilter(self, object, event):
+        if event.type()== QEvent.WindowDeactivate or event.type()== QEvent.FocusOut:
+            if (not willExitOnItsOwn):
+                quitNicely()
+        return False
+
+# clickable label
+class QClickableLabel(QLabel):
+    clicked=pyqtSignal(str)
+    def __init__(self, parent=None):
+        QLabel.__init__(self, parent)
+
+    def mousePressEvent(self, ev):
+        self.clicked.emit(self.text())        
+    
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = EmojiPickerWindow()
+    sys.exit(app.exec_())
